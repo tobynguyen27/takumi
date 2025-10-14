78778b52f7b84e68ac64cbf2d689216948697db4
Kane Wang <kane@yeecord.com>
2025-10-14 22:07:29 +0800

    Docs: fix home page og

--- docs/app/routes/home.tsx
+++ docs/app/routes/home.tsx
@@ -3,30 +3,30 @@ import { Link } from "react-router";
 import { Button } from "~/components/ui/button";
 import { baseOptions } from "~/layout-config";
 
-export function meta() {
-  return [
-    { title: "Takumi: Craft Beautiful Images with Code" },
-    {
-      name: "description",
-      content:
-        "A library for generating images using CSS Flexbox layout. Available for Rust, Node.js, and WebAssembly.",
-    },
-  ];
-}
-
 export default function Home() {
   return (
     <HomeLayout className="text-center" {...baseOptions}>
-      <head>
-        <meta
-          name="og:image"
-          content="https://raw.githubusercontent.com/kane50613/takumi/master/example/twitter-images/output/og-image.png"
-        />
-        <meta
-          name="twitter:image"
-          content="https://raw.githubusercontent.com/kane50613/takumi/master/example/twitter-images/output/og-image.png"
-        />
-      </head>
+      <title>Takumi makes dynamic image rendering simple.</title>
+      <meta
+        name="description"
+        content="Production-ready library to make rendering performant, portable and scalable."
+      />
+      <meta
+        name="og:title"
+        content="Takumi makes dynamic image rendering simple"
+      />
+      <meta
+        name="og:description"
+        content="Production-ready library to make rendering performant, portable and scalable."
+      />
+      <meta
+        name="og:image"
+        content="https://raw.githubusercontent.com/kane50613/takumi/master/example/twitter-images/output/og-image.png"
+      />
+      <meta
+        name="twitter:image"
+        content="https://raw.githubusercontent.com/kane50613/takumi/master/example/twitter-images/output/og-image.png"
+      />
       <div className="max-w-5xl w-full mx-auto">
         <div className="flex flex-col py-24 px-4 items-center justify-center">
           <img src="/logo.svg" className="w-16 h-auto" alt="Takumi Logo" />
