a840e4fc9cc83c26029afcfe00d48130a27be6f7
Kane Wang <kane@yeecord.com>
2025-11-11 04:07:32 +0800

    NAPI: update dts header file

--- takumi-napi-core/src/dts-header.d.ts
+++ takumi-napi-core/src/dts-header.d.ts
@@ -1,11 +1,16 @@
 export interface FontDetails {
   name?: string;
-  data: Buffer | ArrayBuffer;
+  data: Uint8Array | ArrayBuffer;
   weight?: number;
-  style?: "normal" | "italic" | "oblique";
+  style?:
+    | "normal"
+    | "italic"
+    | "oblique"
+    | `oblique ${number}deg`
+    | (string & {});
 }
 
-export type Font = FontDetails | Buffer | ArrayBuffer;
+export type Font = FontDetails | Uint8Array | ArrayBuffer;
 
 export interface AnyNode {
   type: string;
