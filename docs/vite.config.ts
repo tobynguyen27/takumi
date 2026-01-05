6ac0ffe0e91d9ac0f8084ae87b1306122bf570bb
Kane Wang <kane@yeecord.com>
2026-01-06 01:55:12 +0800

    Docs: avoid hot-link images

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -2,6 +2,7 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "fumadocs-mdx/vite";
 import { defineConfig } from "vite";
+import remoteAssets from "vite-plugin-remote-assets";
 import tsconfigPaths from "vite-tsconfig-paths";
 import * as MdxConfig from "./source.config";
 
@@ -9,5 +10,11 @@ export default defineConfig({
   ssr: {
     external: ["@takumi-rs/image-response", "typescript", "twoslash", "shiki"],
   },
-  plugins: [mdx(MdxConfig), tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    mdx(MdxConfig),
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    remoteAssets(),
+  ],
 });
