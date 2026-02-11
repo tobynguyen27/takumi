d6d21110baed24432db23794bfc988b25f99649a
Kane Wang <kane@yeecord.com>
2026-02-11 23:01:18 +0800

    Helpers: remove unused build script

--- takumi-helpers/build.ts
+++
@@ -1,9 +0,0 @@
-import dts from "bun-plugin-dts";
-
-await Bun.build({
-  entrypoints: ["./src/index.ts", "./src/jsx/jsx.ts"],
-  outdir: "./dist",
-  plugins: [dts()],
-  minify: true,
-  packages: "external",
-});
