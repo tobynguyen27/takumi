098f9183e83eb5eb3c8edd1e929d68b91e51516c
Kane Wang <kane@yeecord.com>
2026-02-14 22:19:14 +0800

    Docs: make table searchable

--- docs/source.config.ts
+++ docs/source.config.ts
@@ -1,4 +1,7 @@
-import { rehypeCodeDefaultOptions } from "fumadocs-core/mdx-plugins";
+import {
+  defaultStringifier,
+  rehypeCodeDefaultOptions,
+} from "fumadocs-core/mdx-plugins";
 import {
   defineConfig,
   defineDocs,
@@ -22,9 +25,36 @@ export const docs = defineDocs({
   },
 });
 
+const structureStringifier = defaultStringifier({
+  handlers: {
+    table: (node, _, state, info) => state.containerFlow(node, info),
+    tableRow: (node, _, state, info) => state.containerFlow(node, info),
+    tableCell: (node, _, state, info) => state.containerPhrasing(node, info),
+  },
+});
+
 export default defineConfig({
   plugins: [lastModified()],
   mdxOptions: {
+    remarkStructureOptions: {
+      mdxTypes: (node) =>
+        node.name === "td" ||
+        node.name === "th" ||
+        !node.children ||
+        node.children.length === 0,
+      stringify(node, ctx) {
+        return structureStringifier.call(
+          this,
+          node.type === "mdxJsxFlowElement" &&
+            (node.name === "td" || node.name === "th")
+            ? ({ ...node, type: "mdxJsxTextElement" } as Parameters<
+                typeof structureStringifier
+              >[0])
+            : node,
+          ctx,
+        );
+      },
+    },
     rehypeCodeOptions: {
       themes: {
         light: "github-light",
