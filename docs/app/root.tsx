e75ff46e9788e8c761d705c2709ee88124dbce86
Kane Wang <kane@yeecord.com>
2026-02-14 21:07:41 +0800

    Docs: use Geist font

--- docs/app/root.tsx
+++ docs/app/root.tsx
@@ -10,6 +10,7 @@ import {
 } from "react-router";
 import type { Route } from "./+types/root";
 import "./app.css";
+import { SearchDialog } from "components/search";
 
 export const links: Route.LinksFunction = () => [
   { rel: "icon", type: "image/svg+xml", href: "/logo.svg" },
@@ -21,7 +22,7 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Geist+Mono:wght@100..900&family=Geist:wght@100..900&display=swap",
   },
 ];
 
@@ -62,7 +63,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <ReactRouterProvider>
           <RootProvider
             search={{
-              SearchDialog: () => <div />,
+              SearchDialog,
             }}
           >
             {children}
